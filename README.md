10d4640969f2de7618a6370f2defa83539160926
wenqiang0517 <1529236947@qq.com>
2020-10-30 16:56:54 +0800

    practice day1

--- README.md
+++ README.md
@@ -1,6 +1,4 @@
 # python 全栈系列
-python 基础、进阶、高阶
-前端知识
-Django框架
-1111
-1211
+* python 基础、进阶、高阶
+* 前端知识
+* Django框架
---
+++ day1/2-practice.py
@@ -0,0 +1,31 @@
+# 变量 常量 字符串拼接
+a = 1
+b = 2
+AGE = 10
+num_1 = 'a'
+num_2 = "b"
+num_3 = """
+a
+b
+c
+"""
+print(a + b)
+print(AGE)
+print(num_1 * 3 + "1" + num_2 + "2" + num_3)
+print(type(a), type(num_1))
+
+
+# if else
+'''
+x = int(input("请输入一个数："))
+if x > 100:
+    print("牛皮")
+elif x >= 90:
+    print("A")
+elif x >= 80:
+    print("B")
+elif x >= 70:
+    print("C")
+else:
+    print("D")
+'''
