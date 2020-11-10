972a6d931bc383b37d2c3156a7490ff6a80da514
luwenqiang <1529236947@qq.com>
2020-11-10 18:48:47 +0800

    day04 range

---
+++ day04/06 range.py
@@ -0,0 +1,32 @@
+r = range(10)  # [0,1,2,3,4,5,6,7,8,9]
+# 顾头不顾腚
+# print(r)
+# for i in r:
+#     print(i)
+# 索引（了解）
+# print(r[1])
+
+# for i in range(1,101): print(i)
+
+# for i in range(2,101,2):
+#     print(i)
+
+# for i in range(100,0,-1):
+#     print(i)
+
+# l1 = [1, 2, 3, 'alex', '太白', 2, 3, 4, 66,]
+# # 利用for循环，利用range将l1列表的所有索引依次打印出来
+# '''
+# 0
+# 1
+# 2
+# 3
+# 4
+# '''
+# for i in range(len(l1)):
+#     print(i)
+
+# for i in range(3):
+#     pass
+# print(i)
+
