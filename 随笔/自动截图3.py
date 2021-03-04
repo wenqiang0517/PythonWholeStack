23dd2d21ee094993189bfed951275f1589d9d4e0
wenqaing0517 <1529236947@qq.com>
2021-03-04 18:06:45 +0800

    自动截图3

--- 随笔/自动截图3.py
+++ 随笔/自动截图3.py
@@ -4,24 +4,30 @@ from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 import os
 import time
+
+
 # https://www.cnblogs.com/hanfe1/p/13131583.html
+
+# http://chromedriver.storage.googleapis.com/index.html
+# https://npm.taobao.org/mirrors/chromedriver/
+
 
 def get_image(url, pic_name):
     # chromedriver的路径
-    chromedriver = r"C:\Users\name\AppData\Local\Google\Chrome\Application\chromedriver.exe"
+    chromedriver = r"C:\Program Files\Google\Chrome\Application\chromedriver.exe"
     os.environ["webdriver.chrome.driver"] = chromedriver
     # 设置chrome开启的模式，headless就是无界面模式
     # 一定要使用这个模式，不然截不了全页面，只能截到你电脑的高度
     chrome_options = Options()
     chrome_options.add_argument('headless')
-    driver = webdriver.Chrome(chromedriver, chrome_options=chrome_options)
+    driver = webdriver.Chrome(chromedriver, options=chrome_options,)
     # 控制浏览器写入并转到链接
     driver.get(url)
     time.sleep(1)
     # 接下来是全屏的关键，用js获取页面的宽高，如果有其他需要用js的部分也可以用这个方法
     width = driver.execute_script("return document.documentElement.scrollWidth")
     height = driver.execute_script("return document.documentElement.scrollHeight")
-    print(width, height)
+    # print(width, height)
     # 将浏览器的宽高设置成刚刚获取的宽高
     driver.set_window_size(width, height)
     time.sleep(1)
@@ -32,5 +38,5 @@ def get_image(url, pic_name):
 
 # 你输入的参数
 url = 'https://movie.douban.com/top250'
-pic_name = r'./image.png'
+pic_name = r'./image2.png'
 get_image(url, pic_name)
